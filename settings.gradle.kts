9cb32c678467f1cf7dfdef4314a30804563df46b
larega6-sketch <larega6@gmail.com>
2026-03-12 16:07:00 +1200

    Create settings.gradle.kts for Umarex VPN project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+// settings.gradle.kts file for Umarex VPN
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "umarex-vpn"
